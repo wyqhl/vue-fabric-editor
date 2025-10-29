a3f4485bd569fbe5232c463a6dbdee497be69c5b
houliang <hl198821@qq.com>
2025-10-30 01:00:06 +0800

    fix: 隐藏页面顶部按钮

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -9,7 +9,7 @@
   <div class="home">
     <Layout>
       <!-- 头部区域 -->
-      <Top v-if="state.show" :ruler="state.ruler" @update:ruler="rulerSwitch"></Top>
+      <!--      <Top v-if="state.show" :ruler="state.ruler" @update:ruler="rulerSwitch"></Top>-->
       <Content style="display: flex; height: calc(100vh - 64px); position: relative">
         <!-- 左侧区域 -->
         <Left v-if="state.show"></Left>
